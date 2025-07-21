4d9aa30487b79c4dd14d0fee4e44de18293ef8bb
nwn8 <s582031@nwmissouri.edu>
2025-07-21 09:28:53 -0500

    ok

---
+++ dashboard/app.py
@@ -0,0 +1,95 @@
+import prices
+import pickle
+from shiny import reactive, render
+from shiny.express import ui
+
+
+
+
+
+# ------------------------------------------------
+# This refreshes the page
+# ------------------------------------------------
+
+
+UPDATE_INTERVAL_SECS: int = 120
+
+@reactive.calc()
+def reactive_calc_combined():
+
+    # Invalidate this calculation every UPDATE_INTERVAL_SECS to trigger updates
+    reactive.invalidate_later(UPDATE_INTERVAL_SECS)
+
+    # call the methods to get the data
+    aapl_price=prices.get_aapl_price()
+
+    """
+    with open("aapl_log.pkl","rb") as file2:
+         a=pickle.load(file2)
+
+    a_latest=a[-1]
+    """
+
+    nvda_price=prices.get_nvda_price()
+
+    """
+    with open("nvda_log.pkl","rb") as filer:
+        n=pickle.load(filer)
+
+    n_latest=n[-1]
+    """
+    # Get a timestamp for "now" and use string format strftime() method to format it
+    #timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    latest_dictionary_entry = {"aapl": aapl_price, "nvda": nvda_price}
+
+    # Return everything we need (nothing to return)
+    return latest_dictionary_entry
+
+
+
+
+# ------------------------------------------------
+# Define the Shiny UI Page layout - Page Options
+# ------------------------------------------------
+
+
+ui.page_opts(title="Stock Price Page Live", fillable=True)
+
+# ------------------------------------------------
+# Define the Shiny UI Page layout - Sidebar
+# ------------------------------------------------
+
+with ui.sidebar(open="open"):
+    ui.h2("Stock prices", class_="text-center")
+    ui.p(
+        "A demonstration of real-time stock quotes.",
+        class_="text-center",
+    )
+
+
+#---------------------------------------------------------------------
+# In Shiny Express, everything not in the sidebar is in the main panel
+#---------------------------------------------------------------------
+
+
+ui.h2("Current AAPL")
+
+@render.text
+def display_aapl():
+    """Get the latest reading"""
+    latest_entry = reactive_calc_combined()
+    return f"{latest_entry['aapl']}"
+
+
+
+
+ui.hr()
+
+ui.h2("Current NVDA")
+
+@render.text
+def display_nvda():
+    """Get the latest reading"""
+    latest_entry_n = reactive_calc_combined()
+    return f"{latest_entry_n['nvda']}"
---
+++ dashboard/prices.py
@@ -0,0 +1,63 @@
+
+import requests
+#from dotenv import load_dotenv
+import pickle
+
+#load_dotenv()
+
+#api_key=os.getenv("NEW_API_KEY")
+
+def get_aapl_price():
+    url=f"https://api.twelvedata.com/price?symbol=AAPL&apikey=6bf3bdff97a14db19bdea24ba1900629"
+    response = requests.get(url)
+
+    #print(f"Status Code: {response.status_code}")
+    #print(f"Response Body: {response.json()}")
+
+    price_aapl=response.json()
+ 
+    """
+    The following is to create a list of ongoing prices saved as a pickle file
+
+    price_aa=round(float(price_aapl['price']),2)
+    with open("aapl_log.pkl","rb") as file2:
+         a=pickle.load(file2)
+
+    
+    a.append(price_aa)
+    
+    with open("aapl_log.pkl", "wb") as file:
+        pickle.dump(a, file)
+
+    """
+    print(price_aapl)
+    return price_aapl['price']
+
+def get_nvda_price():
+    url=f"https://api.twelvedata.com/price?symbol=nvda&apikey=6bf3bdff97a14db19bdea24ba1900629"
+    response = requests.get(url)
+
+    #print(f"Status Code: {response.status_code}")
+    #print(f"Response Body: {response.json()}")
+
+    price_nvda=response.json()
+
+    """
+    The following is to create and read a list as a pickle file for ongoing prices
+
+    price_nn=round(float(price_nvda['price']),2)
+    with open("nvda_log.pkl","rb") as filer:
+         x=pickle.load(filer)
+
+    x.append(price_nn)
+    
+
+    with open("nvda_log.pkl", "wb") as file:
+        pickle.dump(x, file)
+    """
+
+
+    return price_nvda['price']
+
+get_aapl_price()
+get_nvda_price()
